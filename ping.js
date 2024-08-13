20c17737b738bab2d58e5a5f0497aa1337eda8b4
Mr Devil <118550212+sattya1997@users.noreply.github.com>
2024-08-13 18:38:14 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -6,6 +6,12 @@ const app = express();
 // Schedule a cron job to run every 5 minutes
 cron.schedule('*/5 * * * *', () => {
     console.log('Running a task every 5 minutes');
+    try {
+        await axios.get('https://stock-server-qag4.onrender.com/ping');
+        console.log('Pinged the other server successfully');
+  } catch (error) {
+        console.error('Error pinging the other server:', error);
+  }
 });
  
 app.listen(3000, () => {

b07868c4f6ca77ef9815aa9e85ab65c52b2d5bbe
Mr Devil <118550212+sattya1997@users.noreply.github.com>
2024-08-13 19:27:17 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cron = require('node-cron');
 const axios = require('axios');
+const cors = require('cors');
  
 const app = express();
+app.use(cors());
+
+
  
 // Schedule a cron job to run every 5 minutes
 cron.schedule('*/5 * * * *', async () => {
@@ -12,6 +16,15 @@ cron.schedule('*/5 * * * *', async () => {
         console.log('Pinged the other server successfully');
   } catch (error) {
         console.error('Error pinging the other server:', error);
+  }
+});
+
+app.get("/ping-back", async (req, res) => {
+  try {
+    res.send("ping backed successfully");
+  } catch (error) {
+    console.error("Error fetching Bitsearch API data:", error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
  
